b2ee02ac5af9f6be50e534f6f0596f5222aa6432
Enzo Cussuol <enzo@allsystem.com.br>
2021-06-23 13:04:14 -0300

    Adicao do exemplo OTADHT11

---
+++ Exemplos/OTADHT11.ino
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include <DHT.h>
+#include <ThingSpeak.h>
+#include "DispositivoOTAWS.h"
+
+#define DHTPIN D3
+#define DHTTYPE DHT11
+
+Dispositivo* dispositivo = new Dispositivo(esp8266D1Mini);
+DHT dht(DHTPIN, DHTTYPE);
+
+unsigned long myChannelNumber = 1424234;
+const char * myWriteAPIKey = "FYDWLNURF61W80RB";
+
+WiFiClient client;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Comecando teste do DHT...");
+
+  dht.begin();
+
+  ThingSpeak.begin(client);
+
+  dispositivo->start();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  dispositivo->handle();
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if(isnan(t) || isnan(h)){
+    Serial.println("Falha na leitura do DHT");
+  }else{
+    Serial.print("Umidade: ");
+    Serial.print(h);
+    Serial.print(" %t");
+    Serial.print("Temperatura: ");
+    Serial.print(t);
+    Serial.println(" *C");
+  }
+
+  ThingSpeak.setField(1, t);
+  ThingSpeak.setField(2, h);
+  ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+
+  delay(25000);
+}

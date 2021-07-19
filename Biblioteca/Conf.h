b1fa70703b20227fc9dac0e390f13a2220dba229
Enzo Cussuol <enzo@allsystem.com.br>
2021-07-19 10:11:19 -0300

    Testando rm --cached

---
+++ Biblioteca/Conf.h
@@ -0,0 +1,12 @@
+#ifndef CONF_H
+    #define CONF_H
+
+    #define NOME_WIFI "Claudio"
+    #define SENHA_WIFI "adgj1234"
+
+    #define IP_SERVIDOR 1921688641
+
+    //Topicos nos quais o dispositivo se inscreve
+    #define TOPICO_1 "teste"
+    //...
+#endif

fe5be72b2fb7df4e7e10fbefd1871328781552e2
Enzo Cussuol <enzo@allsystem.com.br>
2021-07-19 10:04:07 -0300

    Atualizacao gitignore

--- Biblioteca/Conf.h
+++ Biblioteca/Conf.h
@@ -1,12 +1,12 @@
 #ifndef CONF_H
     #define CONF_H
 
-    #define NOME_WIFI XXXXX
-    #define SENHA_WIFI XXXXX
+    #define NOME_WIFI "Claudio"
+    #define SENHA_WIFI "adgj1234"
 
-    #define IP_SERVIDOR XXXXX
+    #define IP_SERVIDOR 1921688641
 
     //Topicos nos quais o dispositivo se inscreve
-    #define TOPICO_1 XXXXX
+    #define TOPICO_1 "teste"
     //...
 #endif

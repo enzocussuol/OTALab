099ab7a337c265596bef585f6e0c1d951f135a43
Enzo Cussuol <enzo@allsystem.com.br>
2021-07-20 09:40:56 -0300

    Atualizacao script de gerar ips

--- Scripts/geraArquivoIps.sh
+++ Scripts/geraArquivoIps.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Gera um arquivo ips.txt com todos os ips da rede
+# $1 representa a rede do usuário
 
 rm ~/ProjetoOTA/Relatorios/ips.txt # Remove, se existir, o arquivo de ips antigo
 
 for counter in $(seq 2 254);
 do
-    echo "192.168.86.$counter" >> ~/ProjetoOTA/Relatorios/ips.txt # Simplesmente printa em ips.txt todos os ips da rede
+    echo "$1.$counter" >> ~/ProjetoOTA/Relatorios/ips.txt # Simplesmente printa em ips.txt todos os ips da rede
 done

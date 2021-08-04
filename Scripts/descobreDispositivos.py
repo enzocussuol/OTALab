2896659064dd793da9941d3af0726140f53267c0
Enzo Cussuol <enzo@allsystem.com.br>
2021-08-04 09:49:28 -0300

    Update descobreDispositivos.py

--- Scripts/descobreDispositivos.py
+++ Scripts/descobreDispositivos.py
@@ -23,11 +23,11 @@ def on_message(client, userdata, message):
     arqDisp.write(str(message.payload.decode("utf-8")) + "\n")
 
 try:
-    os.remove("~/OTA-Multiplos-Dispositivos/Relatorios")
+    os.remove("~/OTA-Multiplos-Dispositivos/Relatorios/dispositivos.txt")
 except:
     print("Nao foi possivel remover o arquivo dispositivos.txt (ja nao existe)")
     
-arqDisp = open("~/OTA-Multiplos-Dispositivos/Relatorios", "a")
+arqDisp = open("~/OTA-Multiplos-Dispositivos/Relatorios/dispositivos.txt", "a")
 
 client = mqttClient.Client("Python3")
 # client.username_pw_set(user, password=password)

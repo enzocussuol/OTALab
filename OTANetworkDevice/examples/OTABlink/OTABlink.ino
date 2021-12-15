bceb6c5af109423619cfacc6a35af8359a55d76d
enzocussuol <enzo@allsystem.com.br>
2021-12-15 00:11:16 -0300

    voltando ao github

---
+++ OTANetworkDevice/examples/OTABlink/OTABlink.ino
@@ -0,0 +1,25 @@
+#include <OTANetworkDevice.h>
+
+OTANetworkDevice* device = new OTANetworkDevice();
+
+void setup(){
+    device->setWiFiNetworkName(WIFI_NETWORK_NAME);
+    device->setWiFiNetworkPassword(WIFI_NETWORK_PASSWORD);
+    device->setBrokerIP(BROKER_IP);
+    device->setName(DEVICE_NAME);
+    device->setup();
+
+    Serial.begin(115200);
+    Serial.println("Comecando...");
+    
+    pinMode(2, OUTPUT);
+}
+
+void loop(){
+    device->handle();
+
+    digitalWrite(2, HIGH);
+    delay(5000);
+    digitalWrite(2, LOW);
+    delay(5000);
+}

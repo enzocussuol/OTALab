9bf304f40f7c346a538afc93313feb12bb7d2573
enzocussuol <enzo@allsystem.com.br>
2022-01-20 14:35:38 -0300

    exemplos ESP32

---
+++ OTANetworkDevice/examples/ESP32/Blink/Blink.ino
@@ -0,0 +1,30 @@
+/*
+  ESP 32 Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+  The ESP32 has an internal blue LED at D2 (GPIO 02)
+*/
+
+#include <OTANetworkDevice.h>
+
+OTANetworkDevice* device = new OTANetworkDevice();
+
+int LED_BUILTIN = 2;
+
+void setup() {
+    device->setWiFiNetworkName(WIFI_NETWORK_NAME);
+    device->setWiFiNetworkPassword(WIFI_NETWORK_PASSWORD);
+    device->setBrokerIP(BROKER_IP);
+    device->setName(DEVICE_NAME);
+    device->setup();
+
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+    device->handle();
+
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(1000);                       // wait for a second
+}
